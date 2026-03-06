97ff083dbd63fde379d4ef6808d946b45cfc2419
Moaaz Shah <muaazbinmudassar451@gmail.com>
2026-03-06 19:59:55 +0500

    :

--- my.sql
+++ my.sql
@@ -1,11 +1,21 @@
 CREATE TABLE MonthlySales (
     SalespersonName VARCHAR(255) NOT NULL PRIMARY KEY,
-    RevenueGenerated DECIMAL(10, 2) NOT NULL
+    RevenueGenerated DECIMAL(15, 2) NOT NULL
 );
+
 INSERT INTO MonthlySales (SalespersonName, RevenueGenerated) VALUES
-('Alice', 5000.00),
-('Bob', 7000.00),
-('Charlie', 6000.00),
-('Diana', 8000.00),
-('Eve', 5500.00);
+('Alice', 1500000.00),
+('Bob', 1000000.00),
+('Charlie', 600000.00),
+('Diana', 300000.00),
+('Eve', 550000.00);
 SELECT * FROM MonthlySales;
+
+SELECT SalespersonName, RevenueGenerated,
+CASE 
+WHEN RevenueGenerated > 1000000 THEN 'Star Performer'
+WHEN RevenueGenerated BETWEEN 500000 AND 1000000 THEN 'Solid Performer'
+ELSE 'Need Improvement' END AS PerformanceCategory
+FROM MonthlySales;
+
+DROP TABLE MonthlySales;
